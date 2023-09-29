657b46905d36fb590a4ff101dedfcf68910e1329
HALINANT <antoine.halin@fujitsu.com>
2023-09-29 11:26:17 -0400

    misc changes

--- header-title.js
+++ header-title.js
@@ -1,10 +1,10 @@
 class HeaderTitle extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <div id=container>
+        <div id=title-container>
             <p>Does it&nbsp;</p>
             <div id=flip>
-                <div><div>&lt;spatial predicate&gt;</div></div>
+                <div><div class="title-accent">&lt;spatial predicate&gt;</div></div>
                 <div><div>cross</div></div>
                 <div><div>touch</div></div>
                 <div><div>contain</div></div>
